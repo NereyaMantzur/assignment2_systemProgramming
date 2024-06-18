d6cf1e0eb85210bc9b81e0d935000f528716c912
SimonF <66910708+smnman@users.noreply.github.com>
2024-06-18 21:47:21 +0300

    Update Supermarket.c
    Added Function implements

--- Supermarket.c
+++ Supermarket.c
@@ -16,3 +16,18 @@ int isSupermarketCode(SuperMarket* superPtr, int superCode)
 {
 	return 1;
 }
+
+// Check edit by Simon 
+
+// Function Implementations
+int isSameSupermarket(Supermarket* s1, Supermarket* s2) {
+    return strcmp(s1->name, s2->name) == 0;
+}
+
+int isSupermarketName(Supermarket* s, const char* name) {
+    return strcmp(s->name, name) == 0;
+}
+
+int isSupermarketCode(Supermarket* s, int code) {
+    return s->code == code;
+}
