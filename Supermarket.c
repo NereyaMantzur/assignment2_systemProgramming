1bb8aca91fcde516cd298f5874a0e643e1508442
SimonF <66910708+smnman@users.noreply.github.com>
2024-06-21 16:47:06 +0300

    Update Supermarket.c
    edited functions Comparison.

--- Supermarket.c
+++ Supermarket.c
@@ -17,17 +17,42 @@ int isSupermarketCode(SuperMarket* superPtr, int superCode)
 	return 1;
 }
 
-// Check edit by Simon 
 
 // Function Implementations
-int isSameSupermarket(Supermarket* s1, Supermarket* s2) {
-    return strcmp(s1->name, s2->name) == 0;
+
+int isSameSuperMarket(SuperMarket* super1Ptr, SuperMarket* super2Ptr) {
+    // Check if Input is Null
+    if (super1Ptr == NULL || super2Ptr == NULL) {
+        return 0; 
+    }
+    // compare names
+    if (strcmp(super1Ptr->name, super2Ptr->name) == 0) {
+        return 1; 
+    } else {
+        return 0; 
+    }
 }
 
-int isSupermarketName(Supermarket* s, const char* name) {
-    return strcmp(s->name, name) == 0;
+
+int isSupermarketName(SuperMarket* superPtr, const char* superStr) {
+    if (superPtr == NULL || superStr == NULL) {
+        return 0; 
+    }
+    if (strcmp(superPtr->name, superStr) == 0) {
+        return 1; 
+    } else {
+        return 0; 
+    }
 }
 
-int isSupermarketCode(Supermarket* s, int code) {
-    return s->code == code;
+
+int isSupermarketCode(SuperMarket* superPtr, int superCode) {
+    if (superPtr == NULL) {
+        return 0; 
+    }
+    if (superPtr->code == superCode) {
+        return 1; 
+    } else {
+        return 0; 
+    }
 }
